af4cb8678dd9802abc1a869254587ca18cd4a6b1
mRNA <1518926653@qq.com>
2025-07-24 00:20:45 +0800

    feat: 睡眠管理

--- SleepManage/views.py
+++ SleepManage/views.py
@@ -2,6 +2,7 @@ from rest_framework import viewsets, permissions
 from .models import SleepRecord
 from .serializers import SleepRecordSerializer
 from datetime import timedelta, date
+from rest_framework.response import Response
 
 class SleepRecordViewSet(viewsets.ModelViewSet):
     serializer_class = SleepRecordSerializer
@@ -10,13 +11,32 @@ class SleepRecordViewSet(viewsets.ModelViewSet):
     def get_queryset(self):
         user = self.request.user
         qs = SleepRecord.objects.filter(user=user)
-        week = self.request.query_params.get('week')
-        if week:
-            today = date.today()
-            start = today - timedelta(days=today.weekday())
-            end = start + timedelta(days=6)
-            qs = qs.filter(date__range=[start, end])
+        start_date = self.request.query_params.get('start_date')
+        end_date = self.request.query_params.get('end_date')
+        if start_date and end_date:
+            qs = qs.filter(date__range=[start_date, end_date])
         return qs
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
+
+    def list(self, request, *args, **kwargs):
+        queryset = self.filter_queryset(self.get_queryset())
+
+        # 序列化数据
+        serializer = self.get_serializer(queryset, many=True)
+
+        # 自定义响应结构
+        return Response({
+            'code': 0,
+            'message': '获取成功',
+            'data': serializer.data
+        })
+    
+    def create(self, request, *args, **kwargs):
+        response = super().create(request, *args, **kwargs)
+        return Response({
+            'code': 0,
+            'message': '创建成功',
+            'data': response.data
+        })
